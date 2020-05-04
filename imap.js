d42025df0b324c41080d3a1d8fe4f303391c7f00
iwendo <ikuzovnikov@gmail.com>
2020-05-04 21:34:42 +0500

    Method changes

--- imap.js
+++ imap.js
@@ -1,7 +1,7 @@
 
 //Часть скрипта для показа/скрытия карты через display = none
-let hidemap = document.querySelector(".hidemap");
-let imap = document.querySelector(".imap");
+let hidemap = document.getElementById("hidemap");
+let imap = document.getElementById("imap");
 hidemap.onclick = function () 
 {
     if (imap.style.display === "none") 
@@ -16,16 +16,16 @@ hidemap.onclick = function ()
 };
 
 //Часть скрипта для работы с .svg, задаем переменные svgstyles - все слои с классом hidden, p - подпись сверху для информации о текущем помещении, tooltip - всплывающая подсказка
-let svgstyles = document.querySelectorAll('svg .hidden');
-let p = document.querySelector ("p");
-let tooltip = document.getElementById('tooltip');
+let svgstyles = document.querySelectorAll("svg .hidden");
+let roomname = document.getElementById ("roomname");
+let tooltip = document.getElementById("tooltip");
 //перебор всех элементов (слоев) .svg с классом hidden
 for (let svgstyle of svgstyles)
 {
 //при наведении мыши на область на элемент (слой) удаляем класс hidden    
     svgstyle.onmouseover = function () 
     {
-        svgstyle.classList.toggle('hidden');
+        svgstyle.classList.toggle("hidden");
         tooltip.textContent = svgstyle.dataset.hint;
         tooltip.style.display = "inline";
     };
@@ -38,15 +38,15 @@ for (let svgstyle of svgstyles)
 //прячем элемент(слой) подсветки конкретного помещения внутри .svg когда курсор уходит с него
     svgstyle.onmouseout = function () 
     {
-        svgstyle.classList.toggle('hidden');
-        tooltip.style.display = "none"
+        svgstyle.classList.toggle("hidden");
+        tooltip.style.display = "none";
     };
 //меняем изображение при клике на конкретный элемент(слой) из тех, что в .svg
     svgstyle.onclick = function () 
     {
         viewimage.src = "image/"+svgstyle.id+".png"; //картинки или другие объекты называем также как и слои при создании .svg в фотошопе
         viewimage.alt = svgstyle.dataset.hint;
-        p.textContent = svgstyle.dataset.hint;
+        roomname.textContent = svgstyle.dataset.hint;
     };
 };
 
